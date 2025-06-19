10103755b25be3a99d3226c9cfc667d3f15a349e
asif2508 <rakibul4210@gmail.com>
2025-06-19 10:36:42 +0800

    user sign in signup and other apis

--- app/users/user.service.js
+++ app/users/user.service.js
@@ -1,22 +1,83 @@
-const User = require("./user.model")
+const User = require("./user.model");
+const bcrypt = require("bcrypt");
+const createUser = async (payload) => {
+  const { name, email, password, role } = payload;
+  const isExist = await User.findOne({ email: email });
+  if (isExist) {
+    throw new Error("User already exist. Please use a different email!");
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-const createUser = async(payload)=>{
-    const {name, email, password, role} =payload
+  const result = await User.create({ name, email, password: hashedPassword, role });
+  if (!result) {
+    throw new Error("Failed to create the user");
+  }
+  return result;
+};
 
-    // Your task: 
-    // 1. check if user already exist
-    // 2. if user exist return "User already exist"
-    // 3. if user does not exist create user
-    // 4. hash the password and save the hashed version
-    const result = await  User.create({name, email, password, role})
+const signin = async (payload) =>{
+    const {email, password} = payload
+
+    // checking if email and password both are available
+    if(!email || !password){
+        throw new Error("Please provide email and password")
+    }
+
+
+    // if the user exist
+    const isExist = await User.findOne({email})
+    if(!isExist){
+        throw new Error("Email is wrong!")
+    }
+
+
+    // checking if the password is correct
+    const isPasswordCorrect = await bcrypt.compare(password, isExist.password)
+
+    if(!isPasswordCorrect){
+        throw new Error("Password is wrong!")
+    }
+
+    return isExist
+
+}
+
+const getAllUsers = async () =>{
+    const result = await User.find({}).sort({createdAt: -1})
+    return result
+}
+
+const getUserById = async(id) =>{
+    const result = await User.findById(id)
+    return result
+}
+
+const deleteUserById = async(id) =>{
+    const result = await User.findByIdAndDelete(id)
+
     if(!result){
-        return "Failed to create user"
+        throw new Error("Failed to delete the user!")
+    }
+
+    return result
+}
+
+const updateUserById = async(id, payload) => {
+    const result = await User.findByIdAndUpdate(id, payload, {new: true})
+    if(!result){
+        throw new Error("Failed to update the user!")
     }
     return result
 }
 
-const UserService ={
-    createUser
-}
 
-module.exports = UserService
+const UserService = {
+  createUser,
+  signin,
+  getAllUsers,
+  getUserById,
+  deleteUserById,
+  updateUserById
+};
+
+module.exports = UserService;
