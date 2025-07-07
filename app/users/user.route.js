4ce0236e0d5e2e1ca6047d5628b514191a72015c
asif2508 <rakibul4210@gmail.com>
2025-07-07 15:16:02 +0600

    autorization

--- app/users/user.route.js
+++ app/users/user.route.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 router.post("/create-user", UserControllers.createUser)
 router.post("/signin", UserControllers.signin)
-router.get("/get-all-users", UserControllers.getAllUsers)
-router.get("/auto-login", auth ,UserControllers.autoLogin)
+router.get("/get-all-users", auth(['admin']) ,UserControllers.getAllUsers)
+router.get("/auto-login", auth(['admin', 'customer']) ,UserControllers.autoLogin)
 router.get("/get-user-by-id/:id", UserControllers.getUserById)
-router.delete("/delete-user-by-id/:id", UserControllers.deleteUserById)
-router.patch('/update-user-by-id/:id', UserControllers.updateUserById)
+router.delete("/delete-user-by-id/:id", auth(['admin']) ,UserControllers.deleteUserById)
+router.patch('/update-user-by-id/:id', auth(['admin', 'customer']) ,UserControllers.updateUserById)
 module.exports = router
 
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,23 +1,33 @@
-const jwt = require("jsonwebtoken")
-const auth = (req, res, next) => {
-    try{
-const  token = req.headers.authorization;
-    console.log("here token",token)
-    if(!token){
+const jwt = require("jsonwebtoken");
+const auth = (roles) => {
+  return (req, res, next) => {
+    try {
+      const token = req.headers.authorization;
+      console.log("here token", token);
+      if (!token) {
         return res.status(401).json({
-            success: false,
-            message: "Unauthorized"
-        })
+          success: false,
+          message: "Unauthorized",
+        });
+      }
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      //   console.log("decoded",decoded)
+      //   console.log("roles", roles)
+      if (!roles.includes(decoded.role)) {
+        return res.status(403).json({
+          success: false,
+          message: "Forbidden Access",
+        });
+      }
+      req.user = decoded;
+      next();
+    } catch (err) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden Access",
+      });
     }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded
-    next();
-    }catch(err){
-       return res.status(403).json({
-            success: false,
-            message: "Forbidden Access"
-        })
-    }
-}
+  };
+};
 
-module.exports = auth
+module.exports = auth;
